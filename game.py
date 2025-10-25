44f4bdf44a95acee23227f070de4d246743054f8
Olateju17 <oladitanolateju@gmail.com>
2025-10-25 16:35:33 +0100

    Add files via upload

---
+++ game.py
@@ -0,0 +1,10 @@
+# Program to check student eligibility for Raj's class
+
+# Get student name and age
+name = input("Enter student name: ")
+age = int(input("Enter student age: ")
+          
+if 10 <= age <= 20:
+    print(f"✅ {name} is eligible to enroll in Raj's class.")
+else:
+    print(f"❌ {name} is NOT eligible to enroll in Raj's class.")
